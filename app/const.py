e0b2a7346ed254fa5859b6f18633f8f3642f6306
Bastien <bastien@nizart.me>
2023-01-08 15:44:30 +0100

    change the directory of game file

--- app/const.py
+++ app/const.py
@@ -3,14 +3,11 @@ from enum import Enum
 
 class Game(Enum):
     SCREEN_SIZE = 500
-    PLAYERS = ('x', 'o')
     TOKEN_POSITION = [
         [(50, 50), (200, 50), (350, 50)],
         [(50, 200), (200, 200), (350, 200)],
         [(50, 350), (200, 350), (350, 350)]
     ]
-    TOKEN_IMAGE = ['./assets/cross.png', './assets/circle.png']
-
 
 class Color(Enum):
     WHITE = (255, 255, 255)
